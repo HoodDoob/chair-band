b08546343b1df42202020133ace24a59715db0fb
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-06-05 20:57:26 +0200

    dfgdfg

--- src/pages/korolowo.jsx
+++ src/pages/korolowo.jsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 import Head from "next/head";
 import styles from "../styles/newrelease.module.scss";
-import Layout from "@/components/layout";
 
 export default function NewRelease() {
   return (
-    <div>
+    <>
       <Head>
         <title>Korolowo</title>
       </Head>
-      <Layout></Layout>
-    </div>
+    </>
   );
 }
