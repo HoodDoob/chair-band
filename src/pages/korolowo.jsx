f4df9477ec6128f484e6fc75a43eae4c9aae1052
HoodDoob <hubert.kurkiewicz@gmail.com>
2023-06-05 20:49:55 +0200

    sdfsdf

--- src/pages/korolowo.jsx
+++ src/pages/korolowo.jsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
 import Head from "next/head";
 import styles from "../styles/newrelease.module.scss";
-import Layout from "@/components/layout";
 
 export default function NewRelease() {
   return (
@@ -10,13 +8,13 @@ export default function NewRelease() {
       <Head>
         <title>Korolowo</title>
       </Head>
-      <Layout></Layout>
-      {/* <div className={styles.newrelCont}>
+
+      <div className={styles.newrelCont}>
         <h1>Not yet, actually</h1>
         <h2>
           <Link href="/">Back to home</Link>
         </h2>
-      </div> */}
+      </div>
     </>
   );
 }
